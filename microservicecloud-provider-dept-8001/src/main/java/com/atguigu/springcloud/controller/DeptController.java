d875f8df539bc88e963d768f81eb0d9c3032bece
yanyupingsheng395 <395351080@qq.com>
2019-08-16 18:04:37 +0800

    provider-8001提交

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+    @Autowired
+    DeptService deptService;
+
+
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+    public boolean add(@RequestBody Dept dept)
+    {
+        return deptService.add(dept);
+    }
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") Long id)
+    {
+        return deptService.get(id);
+    }
+
+    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+    public List<Dept> list()
+    {
+        return deptService.list();
+    }
+}
