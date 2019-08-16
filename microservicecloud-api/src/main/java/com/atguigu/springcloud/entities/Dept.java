fd73b7868e2f5e919c6e4ab96a041e66da47d6be
yanyupingsheng395 <395351080@qq.com>
2019-08-16 17:42:58 +0800

    第一次提交

---
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Dept implements Serializable{
+    private Long deptno;
+    private String dname;
+    private String db_source;
+}
